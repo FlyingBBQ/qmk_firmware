c99969a726ce8c41ea4653d5ac15cfad7435510d
FlyingBBQ <derek.cardol@gmail.com>
2020-03-29 01:22:40 +0100

    Add leader key

--- keyboards/ai03/polaris/keymaps/FlyingBBQ/keymap.c
+++ keyboards/ai03/polaris/keymaps/FlyingBBQ/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
          KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
          KC_LSFT, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, MO(_FN),
-         KC_NO  , MO(_FN), KC_LGUI, KC_NO  , KC_SPC , KC_NO  , KC_NO  , KC_RGUI, KC_RALT, KC_NO
+         KC_NO  , MO(_FN), KC_LGUI, KC_NO  , KC_SPC , KC_NO  , KC_NO  , KC_LEAD, KC_RALT, KC_NO
     ),
     [_FN] = LAYOUT_all
     (
@@ -53,15 +53,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-void keyboard_post_init_user(void)
-{
+void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom(); // enables Rgb, without saving settings
     rgblight_sethsv_noeeprom(57, 255, 255); // sets the color without saving
     rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record)
-{
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     bool process_key = true;
     switch (keycode) {
         case MC_GITH:
@@ -81,3 +79,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     }
     return process_key;
 };
+
+LEADER_EXTERNS();
+void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+
+        SEQ_TWO_KEYS(KC_G, KC_H) {
+            SEND_STRING("https://www.github.com/FlyingBBQ\n");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_L) {
+            SEND_STRING("https://gitlab.com/FlyingBBQ\n");
+        }
+        leader_end();
+    }
+}
