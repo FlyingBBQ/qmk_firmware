f55d31c2c39c1978a5caf2fa2984b9236b1cb2ff
Derek Cardol <derek.cardol@zigninnovations.com>
2020-09-22 17:49:00 +0200

    Add winkeys and reformat

--- keyboards/dz60/keymaps/FlyingBBQ/keymap.c
+++ keyboards/dz60/keymaps/FlyingBBQ/keymap.c
@@ -1,5 +1,8 @@
 #include QMK_KEYBOARD_H
 
+#define  WIN_L    LCTL(LGUI(KC_LEFT))
+#define  WIN_R    LCTL(LGUI(KC_RGHT))
+
 enum layer_names {
     _BASE,
     _FN,
@@ -8,18 +11,18 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT
     (
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-        KC_LCTL, MO(_FN), KC_LGUI,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_RALT, KC_RGUI, XXXXXXX, MO(_FN), KC_RCTL
+        KC_GESC, KC_1   , KC_2   , KC_3  , KC_4  , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , XXXXXXX, KC_BSPC,
+        KC_TAB , KC_Q   , KC_W   , KC_E  , KC_R  , KC_T  , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_LCTL, KC_A   , KC_S   , KC_D  , KC_F  , KC_G  , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
+        KC_LSFT, XXXXXXX, KC_Z   , KC_X  , KC_C  , KC_V  , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_DEL,
+        MO(_FN), KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI, XXXXXXX, MO(_FN), KC_RCTL
     ),
     [_FN] = LAYOUT
     (
-         KC_GRV,   KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, KC_F12,  ______, KC_DEL,
-         ______,  ______, KC_PGUP, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,  ______, ______,   RESET,
-        KC_CAPS, KC_HOME, KC_PGDN, KC_END, ______, ______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  ______,  ______, ______,
-         ______,  ______,   WIN_L,  WIN_R, ______, ______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MUTE, ______, KC_PSCR,
-         ______,  ______,  ______, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,
+        KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
+        _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET  ,
+        KC_CAPS, KC_HOME, KC_PGDN, KC_END , _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, _______,
+        _______, _______, WIN_L  , WIN_R  , _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MUTE, _______, KC_PSCR,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
