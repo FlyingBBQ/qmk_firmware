d3d443092023d57cf62f2326e11afc467ab87e53
Derek Cardol <derek.cardol@zigninnovations.com>
2020-09-22 17:31:06 +0200

    Add custom keymap

---
+++ keyboards/dz60/keymaps/FlyingBBQ/keymap.c
@@ -0,0 +1,25 @@
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _BASE,
+    _FN,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT
+    (
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
+        KC_LCTL, MO(_FN), KC_LGUI,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_RALT, KC_RGUI, XXXXXXX, MO(_FN), KC_RCTL
+    ),
+    [_FN] = LAYOUT
+    (
+         KC_GRV,   KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, KC_F12,  ______, KC_DEL,
+         ______,  ______, KC_PGUP, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,  ______, ______,   RESET,
+        KC_CAPS, KC_HOME, KC_PGDN, KC_END, ______, ______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  ______,  ______, ______,
+         ______,  ______,   WIN_L,  WIN_R, ______, ______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MUTE, ______, KC_PSCR,
+         ______,  ______,  ______, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,
+    ),
+};
---
+++ keyboards/dz60/keymaps/FlyingBBQ/readme.md
@@ -0,0 +1,4 @@
+# FlyingBBQ - dz60
+
+compile: `make dz60:FlyingBBQ`
+flash: `sudo make dz60:FlyingBBQ:dfu`
